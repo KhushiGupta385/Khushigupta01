c700e7f8e78ae6bfe85342c45bfc81e0fcce5e8a
Khushi Gupta <khushisatna385@gmail.com>
2026-01-09 22:08:32 +0530

    Add files via upload

---
+++ JAVA6/First10Natural.java
@@ -0,0 +1,8 @@
+// Print first 10 natural numbers.
+public class First10Natural {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
